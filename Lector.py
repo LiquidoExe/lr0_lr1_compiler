9066f3773fd5f302556697c0cb7013362120362d
liquidoexe <cruzizaat@gmail.com>
2020-06-23 21:01:20 -0500

    Se agregaron algunos cambios en los prints.

--- Lector.py
+++ Lector.py
@@ -17,7 +17,6 @@ class lector:
 		print("OBTENIENDO TERMINALES:")
 		for regla in self.conjunto_reglas:
 			self.lt.append(self.obtener_subcadena(regla,'->'))
-		print(self.lt)
 	#Obtener los no terminales de las reglas:
 	def obtener_no_terminales(self):
 		l_aux=[]
@@ -35,14 +34,14 @@ class lector:
 		nt=[]
 		for r_nt in l_aux:
 			nt+=r_nt.split('|')
-		print(nt)
+
 		l_aux=nt
 		nt=[]
 		for elemento in l_aux:
-			print(elemento.split())
+			if elemento.split() != []:
+				nt+=elemento.split()
 
-
-
+		self.ln=nt
 	#Obtener el siguiente pedazo de cadena antes de un símbolo:
 	def obtener_subcadena(self,cadena,simbolo):
 
@@ -55,3 +54,5 @@ leer_reglas=lector()
 leer_reglas.obtener_reglas('C:\\Users\\Líquido\\github\\Creator_LR0\\Reglas.txt')
 leer_reglas.obtener_terminales()
 leer_reglas.obtener_no_terminales()
+print(leer_reglas.lt)
+print(leer_reglas.ln)
